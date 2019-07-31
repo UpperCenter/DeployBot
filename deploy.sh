7426ef2c73b74b327d3754ced7248ca87b2ff403
Thomas Hindby <tom@uppercenter.co>
2019-07-31 18:47:17 +0100

    Barebones Layout

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#---------------------------------------------------#
+# Made by UpperCenter (https://twitter.com/UpperCenter)
+#---------------------------------------------------#
+
+# A "Simple" script that will allow me to deploy a PHP 7.x / Laravel app to a VPS from Github with next to no config.
+
+# Please don't shout at me saying that there are more efficient ways / better scripts out there that I could use. I don't care.
+
+# If you stumble across this script and don't know what you're doing, I take no responsibility for anything that you mess up.
+
+# Check the "licence" file for more information.
+
+#!/bin/bash
+
+sudo apt update
+
+sudo apt add-repository
